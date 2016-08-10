41fce22ec29f092658e9cc823e4124b0aaad340b
prateeklal <lalprateek01@gmail.com>
2016-08-10 09:02:21 +0530

    Modifying file

--- 3-chessboard.js
+++ 3-chessboard.js
@@ -13,3 +13,22 @@ for (var i = 0; i < 8; i++) {
   }
 }
 console.log(char);
+// :( Not able to solve
+
+
+// Original Answer
+var size = 8;
+
+var board = "";
+
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
+}
+
+console.log(board);
